84423b2484ded06c108893e31cbcc9ebd88bb301
khkang <alkalisummer@gmail.com>
2023-12-25 18:02:57 +0900

    node http mocks 적용

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -5,16 +5,33 @@ const newProduct = require('../data/new-product.json');
 
 productModel.create = jest.fn();
 
+let req, res, next;
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+});
+
 describe('Product Controller Create', () => {
+  beforeEach(() => {
+    req.body = newProduct;
+  });
   it('should have a createProduct function', () => {
     expect(typeof productController.createProduct).toBe('function');
   });
   it('should call ProductModel.create', () => {
-    let req = httpMocks.createRequest();
-    let res = httpMocks.createResponse();
-    let next = null;
-    req.body = newProduct;
     productController.createProduct(req, res, next);
     expect(productModel.create).toBeCalledWith(newProduct);
+  });
+  it('should return 201 response code', () => {
+    productController.createProduct(req, res, next);
+    expect(res.statusCode).toBe(201);
+    console.log('isEndCalled:' + res._isEndCalled());
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+  it('should return json body in response', () => {
+    productModel.create.mockReturnValue(newProduct);
+    productController.createProduct(req, res, next);
+    expect(res._getJSONData()).toStrictEqual(newProduct);
   });
 });
